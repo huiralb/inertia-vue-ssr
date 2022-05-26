bafb2765b34cc3f4311a0e004d7caccb4a336619
Irnovi <huiralb@gmail.com>
2022-05-26 19:57:16 +0700

    install vue3 inertia

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,20 @@
-require('./bootstrap');
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap')
+
+
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/inertia-vue3'
+
+createInertiaApp({
+  resolve: name => require(`./Pages/${name}`),
+  setup({ el, App, props, plugin }) {
+    createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .mount(el)
+  },
+})
